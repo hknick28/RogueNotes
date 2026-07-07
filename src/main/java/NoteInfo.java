a10b74eace389da6fe48249ebaad860d3d84c685
Hriday <patelhrid@ecs.vuw.ac.nz>
2026-07-07 16:53:19 +1200

    added: header to InfoPanel with exit button, back button, and title.

--- src/main/java/NoteInfo.java
+++ src/main/java/NoteInfo.java
@@ -4,25 +4,45 @@ import java.awt.*;
 public class NoteInfo extends JPanel {
   private NoteObject note;
   private JPanel main_panel;
+
   public NoteInfo(NoteObject noteObject, JPanel panel) {
     this.main_panel = panel;
     this.note = noteObject;
     this.setBackground(Color.orange);
-    this.setLayout(new GridLayout(1, 3));
+    this.setLayout(new BorderLayout());
 
-    createCloseWindowButton();
-
+    setupHeader();
     this.setVisible(true);
   }
 
-  private void createCloseWindowButton(){
+  private JButton createCloseWindowButton() {
     JButton closeButton = new JButton("Close");
     closeButton.addActionListener( e -> hideNotePanel() );
-    this.add(closeButton);
+    return closeButton;
   }
 
-  public void hideNotePanel(){ this.setVisible(false); main_panel.remove(this); }
+  private void hideNotePanel(){ this.setVisible(false); main_panel.remove(this); }
 
 
+
+  private void setupHeader(){
+    JPanel headerPanel = new JPanel();
+    headerPanel.setLayout(new BorderLayout());
+
+    JLabel titleLabel = new JLabel("Note Info: " + this.note.name());
+    titleLabel.setHorizontalAlignment(JLabel.CENTER);
+
+    headerPanel.add(createBackButton(), BorderLayout.WEST);
+    headerPanel.add(titleLabel, BorderLayout.CENTER);
+    headerPanel.add(createCloseWindowButton(), BorderLayout.EAST);
+
+    this.add(headerPanel, BorderLayout.NORTH);
+  }
+
+  private JButton createBackButton() {
+    JButton backButton = new JButton("Close");
+    backButton.addActionListener( e -> System.out.println("Back button clicked!") );
+    return backButton;
+  }
 
 }
