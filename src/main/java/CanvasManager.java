097f6f2f450db19d72f87d0eb008a0656c35a4ef
Hriday <patelhrid@ecs.vuw.ac.nz>
2026-07-30 19:56:12 +1200

    refactor: Save and load java zip format instead of json

--- src/main/java/CanvasManager.java
+++ src/main/java/CanvasManager.java
@@ -1,33 +1,78 @@
-import com.fasterxml.jackson.core.StreamReadConstraints;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+import javax.imageio.ImageIO;
 
 public class CanvasManager {
   private final ObjectMapper mapper;
 
   public CanvasManager() {
     this.mapper = new ObjectMapper();
-
-    //temp fix, set max file size
-    StreamReadConstraints constraints = StreamReadConstraints.builder()
-            .maxStringLength(100_000_000)
-            .build();
-
-    this.mapper.getFactory().setStreamReadConstraints(constraints);
     // Formats JSON nicely so it's human-readable
     this.mapper.enable(SerializationFeature.INDENT_OUTPUT);
   }
 
-  // Save Canvas Data to JSON file
-  public void saveCanvas(CanvasData canvasData, File file) throws IOException {
-    mapper.writeValue(file, canvasData);
+  // Save Canvas Data to zip file
+  public void saveCanvas(CanvasData canvasData, BufferedImage bgImage, File file) throws IOException {
+    try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(file))) {
+
+      // Pack JSON metadata into 'data.json'
+      ZipEntry jsonEntry = new ZipEntry("data.json");
+      zos.putNextEntry(jsonEntry);
+      byte[] jsonBytes = mapper.writeValueAsBytes(canvasData);
+      zos.write(jsonBytes);
+      zos.closeEntry();
+
+      // Pack raw background image into 'background.png'
+      if (bgImage != null) {
+        ZipEntry imgEntry = new ZipEntry("background.png");
+        zos.putNextEntry(imgEntry);
+        ImageIO.write(bgImage, "png", zos);
+        zos.closeEntry();
+      }
+    }
   }
 
-  // Load Canvas Data from JSON file
-  public CanvasData loadCanvas(File file) throws IOException {
-    return mapper.readValue(file, CanvasData.class);
+  // Load Canvas Data from zip file
+  public LoadedProject loadCanvas(File file) throws IOException {
+    CanvasData canvasData = null;
+    BufferedImage bgImage = null;
+
+    try (ZipInputStream zis = new ZipInputStream(new FileInputStream(file))) {
+      ZipEntry entry;
+      while ((entry = zis.getNextEntry()) != null) {
+        String entryName = entry.getName();
+
+        if (entryName.equals("data.json")) {
+          canvasData = mapper.readValue(zis, CanvasData.class);
+        } else if (entryName.equals("background.png")) {
+          bgImage = ImageIO.read(zis);
+        }
+        zis.closeEntry();
+      }
+    }
+
+    return new LoadedProject(canvasData, bgImage);
+  }
+
+  public static class LoadedProject {
+    private final CanvasData canvasData;
+    private final BufferedImage bgImage;
+
+    public LoadedProject(CanvasData canvasData, BufferedImage bgImage) {
+      this.canvasData = canvasData;
+      this.bgImage = bgImage;
+    }
+
+    public CanvasData getCanvasData() { return canvasData; }
+    public BufferedImage getBgImage() { return bgImage; }
   }
 }
